0fcca5896cb1a3cb758340e8dc60133e914edee8
sma36 <msaqib661@gmail.com>
2019-10-24 20:22:00 +0530

    twoTierArchitecture assignment

---
+++ twoTierArchitecture(Assignment)/ADOdemo/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace ADOdemo
+{
+    class Program
+    {
+        Employee employee;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+    up:     Console.WriteLine("\n\n1.All Employees\t2.Insert Employee" +
+                "\t3.Update Employee\t4.Delete Employee\t5.Search Employee\t6.Exit\n");
+
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:p.allEmployeeDetails();
+                    break;
+                case 2: p.insertEmployeeDetails();
+                    break;
+                case 3:
+                    p.updateEmployeeDetails();
+                    break;
+                case 4:
+                    p.deleteEmployeeDetails();
+                    break;
+                case 5:
+                    p.printEmployeeDetails();
+                    break;
+
+                case 6: goto end;                  
+
+                default:
+                    Console.WriteLine("Invalid choice. Try again");
+                    goto up;                    
+            }
+            goto up;
+
+        end: Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+        public void insertEmployeeDetails()
+        {
+            employee = new Employee();
+            Console.WriteLine("\nEnter name, gender location and salary");
+            employee.Name = Console.ReadLine();
+            employee.Gender = Console.ReadLine();
+            employee.Location = Console.ReadLine();
+            employee.Salary =int.Parse(Console.ReadLine());
+            int count=employee.insertEmployeeDetails(employee);
+            if (count > 0)
+            {
+                Console.WriteLine("\nInsertion successfull");
+            }
+        }
+
+        public void printEmployeeDetails()
+        {
+            employee = new Employee();
+            Console.WriteLine("\nEnter employee id");
+            employee.Id = int.Parse(Console.ReadLine());
+            employee.searchEmployeeDetails(employee);           
+        }
+
+        public void allEmployeeDetails()
+        {
+            employee = new Employee();
+            employee.allEmployeeDetails();
+        }
+
+        public void updateEmployeeDetails()
+        {
+            employee = new Employee();
+            Console.WriteLine("\nEnter employee id, name, gender location and salary");
+            employee.Id = int.Parse(Console.ReadLine());
+            employee.Name = Console.ReadLine();
+            employee.Gender = Console.ReadLine();
+            employee.Location = Console.ReadLine();
+            employee.Salary = int.Parse(Console.ReadLine());
+            int count= employee.updateEmployeeDetails(employee);
+            if (count > 0)
+            {
+                Console.WriteLine("\nUpdate successfull");
+            }
+        }
+
+        public void deleteEmployeeDetails()
+        {
+            employee = new Employee();
+            Console.WriteLine("\nEnter employee id");
+            employee.Id = int.Parse(Console.ReadLine());
+            int count = employee.deleteEmployeeDetails(employee);
+            if (count > 0)
+            {
+                Console.WriteLine("\nSuccessfully deleted");
+            }
+        }
+    }
+}
---
+++ twoTierArchitecture(Assignment)/DAL/Employee.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Employee
+    {
+        
+        private string name,location,gender;
+        private int salary, id;
+
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        string connString = "data source=IN5CG9292JSR; database=ADOdemo; integrated security=true;";
+
+        public string Name { get => name; set => name = value; }
+        public string Location { get => location; set => location = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public int Salary { get => salary; set => salary = value; }
+        public int Id { get => id; set => id = value; }
+
+        public int insertEmployeeDetails(Employee employee)
+        {
+            con.ConnectionString = connString;
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "insertEmployee";
+            cmd.Parameters.AddWithValue("name", employee.Name);
+            cmd.Parameters.AddWithValue("gender", employee.Gender);
+            cmd.Parameters.AddWithValue("location", employee.Location);
+            cmd.Parameters.AddWithValue("salary", employee.Salary);
+            con.Open();
+            int rowCount = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowCount;
+        }
+        public int updateEmployeeDetails(Employee employee)
+        {
+            con.ConnectionString = connString;
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "updateEmployee";
+            cmd.Parameters.AddWithValue("id", employee.Id);
+            cmd.Parameters.AddWithValue("name", employee.Name);
+            cmd.Parameters.AddWithValue("gender", employee.Gender);
+            cmd.Parameters.AddWithValue("location", employee.Location);
+            cmd.Parameters.AddWithValue("salary", employee.Salary);
+            con.Open();
+            int rowCount = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowCount;
+        }
+
+        public int deleteEmployeeDetails(Employee employee)
+        {
+            con.ConnectionString = connString;
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "deleteEmployee";
+            cmd.Parameters.AddWithValue("id", employee.Id); 
+            
+            con.Open();
+            int rowCount = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowCount;
+        }
+
+        public void searchEmployeeDetails(Employee employee)
+        {
+            con.ConnectionString = connString;
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "searchEmployee";
+            cmd.Parameters.AddWithValue("id", employee.Id);
+                     
+            
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine($"{rdr[0]}\t{rdr[1]}\t{rdr[2]}\t{rdr[3]}\t{rdr[4]}");
+            }
+            
+            con.Close();
+            
+        }
+        public void allEmployeeDetails()
+        {
+            con.ConnectionString = connString;
+            cmd.Connection = con;            
+            cmd.CommandText = "select * from employee";
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine($"{rdr[0]}\t{rdr[1]}\t{rdr[2]}\t{rdr[3]}\t{rdr[4]}");
+            }
+
+            con.Close();
+
+        }
+    }
+}
